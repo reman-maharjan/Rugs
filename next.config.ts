a48d848b2548b5a0e7c3865e3167ee44fbe11e88
remanmhzn008 <mhznremon79@gmail.com>
2025-08-20 10:52:54 +0545

    hbasd

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images:{
-    domains: ['images.rugs.com','images.pexels.com','images.unsplash.com']
+    domains: ['images.rugs.com','images.pexels.com','images.unsplash.com','fakestoreapi.com','i.ibb.co']
   },
     eslint: {
     ignoreDuringBuilds: true,
